e64f083f27a89131c8c61df126c34e38cbb6940f
Jessé Correia <linspw@live.com>
2025-01-20 13:04:26 -0300

    fix: eslint issues

--- src/components/map-render/map-render-collab-layer.tsx
+++ src/components/map-render/map-render-collab-layer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useCallback, useEffect, useState } from 'react';
 import { Marker, Popup, useMapEvent } from 'react-leaflet';
 import * as Y from 'yjs';
